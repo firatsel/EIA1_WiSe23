1ee4a9b70d195d3524c0385f98cc167b517d4b05
firatsel <149698005+firatsel@users.noreply.github.com>
2023-12-09 19:31:52 +0100

    L05-2

--- L05-2/script.js
+++ L05-2/script.js
@@ -2,7 +2,7 @@
 
 let myStories = [
     "Du nimmst gemeinsam mit Archäologen an einer Expedition teil. Ihr befindet euch in einer Ruine und hört seltsame Stimmen. Einige der Teilnehmer meinen die Stimmen von rechts zu hören und andere meinen sie kommen von links. Für welchen Weg entscheidest du dich, rechts oder links?",
-    "Die Stimmen werden immer lauter. Ihr betretet einen düsteren Raum mit einer seltsamen Atmosphäre. Von den Wänden krabbeln große spinnenartige Kreaturen. Langsam aber sicher krabbeln sie auf euch zu. Ein Archäologe wurde bereits von einer Kreatur angegriffen. Was machst du? Wegrennen oder Kämpfen?",
+    "Die Stimmen werden immer lauter. Ihr betretet einen düsteren Raum mit einer seltsamen Atmosphäre. Von den Wänden krabbeln große spinnenartige Kreaturen. Langsam aber sicher krabbeln sie auf euch zu. Ein Archäologe wurde bereits von einer Kreatur angegriffen. Was machst du, wegrennen oder kämpfen?",
     "Die Stimmen werden leiser. Ihr entscheidet euch dafür die Stimmen zu ignorieren und macht weiter mit der Ausgrabung.", 
     "Die Kreaturen werden immer schneller und verfolgen dich. Du findest eine Falltür und versteckst dich drunter. Die Kreaturen verschwinden und du kannst gerade noch so entkommen. Glückwuuuunsch", 
     "Du versuchst gegen sie zu kämpfen, jedoch merkst du schnell, dass sie dir überlegen sind. you dead"
