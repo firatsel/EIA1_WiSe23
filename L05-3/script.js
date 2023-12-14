be8a2b3fbb1b64ebfcc4334dcaa12a955c3ddb64
firatsel <149698005+firatsel@users.noreply.github.com>
2023-12-14 22:35:34 +0100

    L05-3

--- L05-3/script.js
+++ L05-3/script.js
@@ -1,3 +1,5 @@
+// hier definiere ich meine Stories mit index 0, 1, 2, 3
+
 story = [
     "Die Stimmen werden immer lauter. Ihr betretet einen düsteren Raum mit einer seltsamen Atmosphäre. Von den Wänden krabbeln große spinnenartige Kreaturen. Langsam aber sicher krabbeln sie auf euch zu. Ein Archäologe wurde bereits von einer Kreatur angegriffen. Was machst du, wegrennen oder kämpfen?",
     "Die Stimmen werden leiser. Ihr entscheidet euch dafür die Stimmen zu ignorieren und macht weiter mit der Ausgrabung.", 
@@ -5,11 +7,20 @@ story = [
     "Du versuchst gegen sie zu kämpfen, jedoch merkst du schnell, dass sie dir überlegen sind. you dead"
 ]
 
-
+/* const input entspricht beim html dokument der id "inputfield"
+const label entspricht beim html dokument der id "textLabel"
+const output entspricht beim html dokument der id "output"
+*/
 
 const input = document.getElementById("inputfield")
 const label = document.getElementById("textLabel")
 const output = document.getElementById("output")
+
+/* Folgend wird die if-Funktion eingefügt. 
+input.style.display = "none" und label.style.display = "none" sind dafür zuständig, dass keine 
+Eingabe mehr erfolgen kann.
+ input.value = "" sorgt dafür, dass das Eingabefeld nach der Eingabe der Antwort gelöscht wird
+*/
 
 input.addEventListener("keydown", function (event) {
     if (event.key == "Enter"); {
@@ -36,16 +47,3 @@ input.addEventListener("keydown", function (event) {
   }
 })
 
-
-
-//if (myPrompt == "rechts") {
- //   let myPrompt = prompt(myStories[1]);
-//
-//    if (myPrompt == "wegrennen") {
-//        let myPrompt = prompt(myStories[3]);
-//    } else (myPrompt == "kämpfen"); {
-//        let myPrompt = prompt(myStories[4]);
- //  }
-//}   else if (myPrompt == "links") {
- //  myPrompt = prompt(myStories[2]);
-//}
